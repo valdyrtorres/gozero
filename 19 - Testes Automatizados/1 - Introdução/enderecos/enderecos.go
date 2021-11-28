345adf5ab8264cd285d5998849463194090c2e10
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2021-11-27 22:50:42 -0300

    testes

---
+++ 19 - Testes Automatizados/1 - Introdução/enderecos/enderecos.go
@@ -0,0 +1,26 @@
+package enderecos
+
+import "strings"
+
+// TipoDeEndereco verifica se um endereco tem um tipo válido e o retorna
+func TipoDeEndereco(endereco string) string {
+	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
+
+	enderecoEmLetraMinuscula := strings.ToLower(endereco)
+	primeiraPalavraDoEndereco := strings.Split(enderecoEmLetraMinuscula, " ")[0]
+
+	// Não há função nativa no Go para procurar string em slice
+	// Então, vamos fazer um for
+	enderecoTemUmTipoValido := false
+	for _, tipo := range tiposValidos {
+		if tipo == primeiraPalavraDoEndereco {
+			enderecoTemUmTipoValido = true
+		}
+	}
+
+	if enderecoTemUmTipoValido {
+		return strings.Title(primeiraPalavraDoEndereco)
+	}
+
+	return "Tipo Inválido"
+}
