565f7bd2280457cf3770987f20fee071d27fd200
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2022-02-14 08:38:07 -0300

    inserindo no banco de dados

---
+++ 24 - Crud Básico/banco/banco.go
@@ -0,0 +1,22 @@
+package banco
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" // Driver de conexao com o MySQL
+)
+
+func Conectar() (*sql.DB, error) {
+	stringConexao := "golang:golang@tcp(localhost:3316)/devbook?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		return nil, erro
+	}
+
+	return db, nil
+}
---
+++ 24 - Crud Básico/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crud/servidor"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux" // Usado para o router
+)
+
+func main() {
+	// CRUD - CREATE, READ, UPDATE, DELETE
+
+	// CREATE - POST
+	// READ - GET
+	// UPDATE - PUT
+	// DELETE - DELETE
+
+	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
